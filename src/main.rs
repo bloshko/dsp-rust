117684403af9ec897871e57d6e6a9e514e59b9ca
bloshko <mis.bloko@gmail.com>
2023-01-13 22:29:12 +0100

    Add wasm_bindgen

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn get_string_with_prefix(prefix: String) -> String {
+    let formatted_string = format!("{} {}", prefix, "1");
+    return formatted_string
+}
+
 fn main() {
-    println!("Hello, dsp-rust!");
+    let str = get_string_with_prefix(String::from("12"));
+    println!("{}", str)
 }
